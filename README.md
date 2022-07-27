b4f00ad3db14c4817667b59cc86dcf5651fe7753
Ayaka Mikazuki <ayaka@mail.shn.hk>
2022-07-27 11:02:15 +0800

    Update contents

--- README.md
+++ README.md
@@ -1,36 +1,48 @@
-# Video list of daily news broadcast in Shin Neng Hakka
+# 城乡一线（兴宁客家话新闻节目）
 
-## Motivation
+<https://linktr.ee/shangshongridsien>
 
-Shin Neng Hakka (兴宁客家话) is a (sub-)dialect of Hakka spoken in the Xingning City (兴宁市). The 10-minute daily news section broadcast by Xingning Radio and Television Station (兴宁市广播电视台), named _Urban and Rural on One Line_ (城乡一线), is a high-quality resource of Shin Neng Hakka. This repository provides a list of all the videos and their permanent links from 17 Apr 2015 to the present.
+## 简介
 
-## Data format
+城乡一线是兴宁本地的民生新闻栏目，创办于 2008 年 4 月。栏目部始终秉持“关注民生，传递信息”的栏目宗旨，深入群众当中，传递政府的声音，反映百姓的诉求。2017 年，围绕“突出民生栏目的服务功能”丰富节目内容，新开发了食品安全监督岗和市民随手拍板块，既宣传推动了市委市政府倡导的创文创卫工作，又契合了广大市民关注的食品安全和环境卫生的需求，受到了广大群众的喜爱。
 
-Currently the sanitized list, `list-sanitized.csv`, contains over 1,500 entries of videos. This is less than one may expect because the video was not posted on some days or the link expires. On the other hand, the full list, `list.csv`, contains all the entries, including the missing and the expired ones. In most cases, you may not wish to use the full list.
+作为兴宁的主流媒体，城乡一线不仅主动与梅州民生 820、服务 900 等节目对接，而且还结合微信网络题材，挖掘了兴宁托举哥钟汉辉、爱心护士曾丝妮兄妹救人等感人事迹，用饱含真情和激情的文字赞扬真善美，及时宣传发生在我们身边的好人好事，进一步弘扬了社会的正能量。
 
-The data is in CSV format, which consists of 4 columns:
+城乡一线每年播出新闻千余条，帮助群众解决了一大批涉及道路交通、水利环保、扶贫济困、民事维权等公益民生方面的实际问题，为传递正能量、揭露假恶丑、构建和谐社会作出了重要的贡献。未来城乡一线仍将继续发扬兴宁地方特色，聚焦关系民生的问题，以强烈的社会责任感和使命感，及时准确反映老百姓的心声，为老百姓提供贴心实在的服务，全力打造广大人民群众都喜爱的民生新闻品牌栏目。
 
-- `DATE`: The date of the news
-- `POST_URL`: The URL of the original webpage of the news
-- `VIDEO_URL`: The URL of the video, extracted from the original webpage
-- `VIDEO_KEY`: The key of the video file (for backup purpose, see below)
+# 数据格式
 
-## Backup of the videos
+数据包含在两个 CSV 文件 `list.csv` 和 `list-sanitized.csv` 中。
 
-To prevent more videos from becoming inaccessible due to expiry, videos that are currently accessible are [backed up to the Internet Archive](https://archive.org/details/snhakkanews). You can access each video by its `VIDEO_KEY`.
+其中，`list.csv` 包含在本仓库制作时网络上可以访问的全部数据，但其中一部分数据有误，无法下载。`list-sanitized.csv` 只包含可下载的。
 
-## Update
+- `DATE`: 视频日期
+- `POST_URL`: 文章链接
+- `VIDEO_URL`: 视频链接
+- `VIDEO_KEY`: 保存在互联网档案馆的文件名
 
-The full list is manually updated.
+# 制作方法
 
-To check the data integrity of the list after each update:
+## 往期视频
+
+```sh
+cd scripts/scraper_old
+./download_search_results.sh
+python process_search_results.py
+./download_posts.sh
+python process_posts.py
+```
+
+## 最新视频
+
+```sh
+cd scripts/scraper
+python main.py
+```
+
+## 检查列表
 
 ```sh
 python check.py
-```
-
-To generate a new sanitized list:
-
-```sh
 grep -v ERROR list.csv > list-sanitized.csv
 ```
--- scripts/scraper/main.py
+++ scripts/scraper/main.py
@@ -1,22 +1,74 @@
-
 from bs4 import BeautifulSoup
+import datetime
+import os
 import re
 import requests
 from typing import Tuple
 
+if not os.path.exists('last_video_date'):
+    last_video_date = datetime.date(1970, 1, 1)
+else:
+    with open('last_video_date', encoding='utf-8') as f:
+        last_video_date = datetime.date.fromisoformat(f.read())
+
+def download_url(url: str) -> str:
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.text
+
 def title2date(title: str) -> Tuple[int, int]:
     date_str = re.fullmatch(r'城乡一线（(.+)）', title).group(1)
     month, day = re.fullmatch(r'(\d{1,2})月(\d{1,2})日', date_str).group(1, 2)
+
+    month = int(month)
+    day = int(day)
+
     return month, day
 
-response = requests.get('http://www.xingning.gov.cn/jrxn/spxw/cxyx/')
-response.raise_for_status()
-html_str = response.text
+def determine_video_date(month: int, day: int) -> datetime.date:
+    today = datetime.date.today()
+    threshold = datetime.timedelta(days=60)
+    lower_range = today - threshold
+    upper_range = today + threshold
 
-soup = BeautifulSoup(html_str, features='html.parser')
+    for year in range(today.year - 1, today.year + 1 + 1):
+        try:
+            video_date = datetime.date(year, month, day)
+        except ValueError:
+            continue
 
-for li in soup.select('.g_main .list_right .listpicture_box ul li'):
-    title = li.select_one('p').get_text()
-    post_url = li.select_one('a')['href']
-    month, day = title2date(title)
-    print(month, day, post_url)
+        if lower_range < video_date < upper_range:
+            return video_date
+
+    raise ValueError('Cannot determine real date')
+
+def get_video_url(page: str) -> str:
+    match = re.search(r'http.+?gdvideo\.southcn\.com.+?\.mp4', page)
+    if match:
+        return match.group(0).replace('\\', '')
+    raise ValueError('Cannot handle the page')
+
+if __name__ == '__main__':
+    html_str = download_url('http://www.xingning.gov.cn/jrxn/spxw/cxyx/')
+
+    new_posts = []
+
+    for li in BeautifulSoup(html_str, features='html.parser').select('.g_main .list_right .listpicture_box ul li'):
+        title = li.select_one('p').get_text()
+        post_url = li.select_one('a')['href']
+        month, day = title2date(title)
+        video_date = determine_video_date(month, day)
+
+        if video_date <= last_video_date:
+            break
+
+        page = download_url(post_url)
+        video_url = get_video_url(page)
+
+        new_posts.append((video_date, post_url, video_url))
+
+    for video_date, post_url, video_url in reversed(new_posts):
+        print(video_date, post_url, video_url, sep='\t')
+
+    with open('last_video_date', 'w', encoding='utf-8') as f:
+        f.write(str(new_posts[0][0]))
